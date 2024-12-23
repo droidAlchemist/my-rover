944299a4892d085b8f457dd2fc66861f60147d89
Joshua SJ <joshua.sj7@gmail.com>
2024-12-23 18:44:07 +0530

    feat: add lidar dependency rf2o

--- ros2_ws/build.sh
+++ ros2_ws/build.sh
@@ -1,7 +1,7 @@
 cd /home/joshua/my-rover/ros2_ws
 export UGV_MODEL=ugv_rover
 colcon build --packages-select iot_controller
-colcon build --packages-select ldlidar robot_pose_publisher ugv_base_node ugv_interface
+colcon build --packages-select ldlidar rf2o_laser_odometry robot_pose_publisher ugv_base_node ugv_interface
 colcon build --packages-select ugv_bringup ugv_description ugv_tools
 source install/setup.bash 
 
